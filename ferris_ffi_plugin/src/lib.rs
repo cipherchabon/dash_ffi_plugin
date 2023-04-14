f0d47541e6814e55f77528bca2fec7d87fd71bb6
Marcos Alejandro <cypherchabon@gmail.com>
2023-04-14 18:12:48 -0300

    paso_3

--- ferris_ffi_plugin/src/lib.rs
+++ ferris_ffi_plugin/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ffi::c_int;
+use std::thread;
+use std::time::Duration;
+
+#[no_mangle]
+pub extern "C" fn sum(a: c_int, b: c_int) -> c_int {
+    a + b
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn sum_long_running(a: c_int, b: c_int) -> c_int {
+    // Simulate a long running task
+    thread::sleep(Duration::from_millis(5000));
+    a + b
 }
