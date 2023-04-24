6e566898bf657fbc9ff6bf2fd6aed6b61b9a4f8d
Marcos Alejandro <cypherchabon@gmail.com>
2023-04-23 21:04:31 -0300

    fix: clippy

--- ferris_ffi_plugin/build.rs
+++ ferris_ffi_plugin/build.rs
@@ -1,18 +1,18 @@
-use cbindgen;
 use std::env;
 
 fn main() {
     let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let mut config = cbindgen::Config::default();
-
-    config.language = cbindgen::Language::C;
-    config.braces = cbindgen::Braces::SameLine;
-    config.cpp_compat = true;
-    config.style = cbindgen::Style::Both;
+    let config = cbindgen::Config {
+        language: cbindgen::Language::C,
+        braces: cbindgen::Braces::SameLine,
+        cpp_compat: true,
+        style: cbindgen::Style::Both,
+        ..Default::default()
+    };
 
     cbindgen::Builder::new()
-        .with_crate(&cargo_manifest_dir)
+        .with_crate(cargo_manifest_dir)
         .with_config(config)
         .with_no_includes()
         .generate()
